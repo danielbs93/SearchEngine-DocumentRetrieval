0889191eebd65205514d9936f7c4427e75e59aa2
erantoutian <erantoutian@gmail.com>
2019-11-23 04:47:45 +0200

    start working NumParser 23/11 can be more than 2 tokens in the list? the first token will be always number? need to remove all the tokens in the list and put just 1 token?

--- src/Rules/NumParser.java
+++ src/Rules/NumParser.java
@@ -20,6 +20,46 @@ public class NumParser extends Anumbers {
 
     @Override
     public LinkedList<Token> Parse() {
+        Token token;
+        double num;
+        if (tokenList.size() == 1){
+            token = tokenList.getFirst();
+            if (!token.isNumeric()) // need to check? who will call this fun
+                return null;
+            num = new Double(token.getName());
+            if (num < 1000)
+                return tokenList;
+            else if (num < 1000000){
+                num = num/1000;
+                tokenList.getFirst().setName(String.valueOf(num) + "K");
+            }
+            else if (num < 1000000000){
+                num = num/1000000;
+                tokenList.getFirst().setName(String.valueOf(num) + "M");
+            }
+            else if ( num >= 1000000000){
+                num = num/1000000000;
+                tokenList.getFirst().setName(String.valueOf(num) + "B");
+            }
+            else
+                return null;
+            return tokenList;
+        }
+        else if (tokenList.size() == 2){
+            token = tokenList.get(2);
+            if (!token.isNumeric()){
+                if (token.getName() == "Thousand" || token.getName() == "thousand" ){
+                    Token number = tokenList.getFirst();
+
+                }
+                if (token.getName() == "Million" || token.getName() == "million" ){
+
+                }
+                if (token.getName() == "Billion" || token.getName() == "billion" ){
+
+                }
+            }
+        }
         return null;
     }
 }
